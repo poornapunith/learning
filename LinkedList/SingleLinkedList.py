46ade3007766e1a3e010fbaa6f8ef6545a17915d
poornapunith <poornachandra813@gmail.com>
2024-02-15 10:21:14 +0530

    InsertAtEnd method adding

--- LinkedList/SingleLinkedList.py
+++ LinkedList/SingleLinkedList.py
@@ -6,11 +6,24 @@ class Node :
 
 
 class LinkedList :
-    def __init__(self,value) :
-        new_node=Node(value)
-        self.head=new_node
-        self.tail=new_node
-        self.length=1
+    def __init__(self) :
+        self.head=None
+        self.tail=None
+        self.length=0
 
-new_Linked_List = LinkedList(10)
+    def InsertAtEnd(self,value):
+        new_node=Node(value)
+        if(self.head==None):
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+
+
+new_Linked_List = LinkedList()
+new_Linked_List.InsertAtEnd(10)
+new_Linked_List.InsertAtEnd(20)
+print(new_Linked_List.length)
 print(new_Linked_List.head.value)
