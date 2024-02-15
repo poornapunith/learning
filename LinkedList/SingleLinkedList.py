05963befe82fe0cb52f1dd4a8a1e67b05d1bdf6f
poornapunith <poornachandra813@gmail.com>
2024-02-15 10:08:17 +0530

    Linked List Class Creation

--- LinkedList/SingleLinkedList.py
+++ LinkedList/SingleLinkedList.py
@@ -4,6 +4,13 @@ class Node :
         self.value=value
         self.next=None
 
-new_node=Node(10)
-print(new_node)
-print(new_node.value)
+
+class LinkedList :
+    def __init__(self,value) :
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+new_Linked_List = LinkedList(10)
+print(new_Linked_List.head.value)
