75e465783b72fd9be2a0dbb501fd3a7e293849a1
poornapunith <poornachandra813@gmail.com>
2024-03-09 19:49:00 +0530

    Depth First Search Code added

--- Trees/trees.py
+++ Trees/trees.py
@@ -9,3 +9,34 @@ leftchild=node("Hot")
 rightchild=node("Cold")
 newBT.left=leftchild
 newBT.right=rightchild
+
+
+def preOrder(root):
+    if(root==None):
+        return
+    else:
+        print(root.value)
+        preOrder(root.left)
+        preOrder(root.right)
+
+#preOrder(newBT)
+        
+def InOrder(root):
+    if(root==None):
+        return
+    else:
+        InOrder(root.left)
+        print(root.value)
+        InOrder(root.right)
+
+#InOrder(newBT)
+        
+def PostOrder(root):
+    if (root==None):
+        return
+    else:
+        PostOrder(root.left)
+        PostOrder(root.right)
+        print(root.value)
+
+PostOrder(newBT)
